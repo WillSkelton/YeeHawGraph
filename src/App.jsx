cd9282bffb1cb239a2d1d235da0bac37bc3f08b4
Will Skelton <william.f.skelton@gmail.com>
2021-10-20 16:42:55 -0700

    Set up layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,33 @@ import Graph from './Components/Graph/Graph';
 
 document.getElementById('body').style.margin = '0px';
 
+const Styles = () => ({
+	App: {
+		display: 'flex',
+		flexDirection: 'row',
+		width: '100vw',
+		height: '100vh',
+	},
+	SidePanel: {
+		width: '20%',
+		height: '100%',
+		backgroundColor: '#4169E1',
+	},
+	GraphContainer: {
+		width: '80%',
+		height: '100%',
+		backgroundColor: '#222',
+	},
+});
+
 export function App() {
+	const styles = Styles();
 	return (
-		<div className="App" style={{ width: '100vw', height: '100vh', backgroundColor: '#222' }}>
-			<Graph />
+		<div className="App" style={styles.App}>
+			<div className="App" style={styles.SidePanel} />
+			<div className="App" style={styles.GraphContainer}>
+				<Graph />
+			</div>
 		</div>
 	);
 }
