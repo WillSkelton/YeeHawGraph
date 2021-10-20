d5b49571ab6ca79671d9a71f112d70f98bfb66bd
Will Skelton <william.f.skelton@gmail.com>
2021-10-20 14:53:35 -0700

    got basic graph to show

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Graph } from './Components/Graph/Graph';
 
 export function App() {
 	return (
 		<div className="App">
-			<header className="App-header" />
+			<h1>Graph</h1>
+			<Graph />
 		</div>
 	);
 }
---
+++ src/Components/Graph/Graph.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import MermaidReact from 'mermaid-react';
+
+const t = `
+pie title Pets adopted by volunteers
+    "Dogs" : 386
+    "Cats" : 85
+    "Rats" : 15
+`;
+
+export function Graph() {
+	return (
+		<MermaidReact
+			id="test"
+			mmd={t}
+			onClick={() => console.log('test Click')}
+			onRender={svg => console.log('render content', svg)} />
+	);
+}
